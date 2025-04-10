6eb955913eac71acfe3ba9bb2d6f3d513afffef9
lhjwork <jin.come.up.business@gmail.com>
2025-04-10 16:43:00 +0900

    css 수정 완

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = () => {
     <div className="align-center navbar">
       <div className="align-center">
         <h1 className="navbar_heading">CartWish</h1>
-      </div>
-      <div className="navbar_form">
-        <input type="text" className="navbar_search" placeholder="Search Products" />
-        <button type="submit" className="search_button">
-          Search
-        </button>
+        <form className="align-center navbar_form">
+          <input type="text" className="navbar_search" placeholder="Search Products" />
+          <button type="submit" className="search_button">
+            Search
+          </button>
+        </form>
       </div>
     </div>
   );
